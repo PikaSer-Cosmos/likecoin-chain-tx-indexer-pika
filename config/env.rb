a0cf1b92d13e552d74dd87d245bdb6fc09a327e2
PikachuEXE <pikachuexe@gmail.com>
2023-11-10 09:17:51 +0800

    * Update `LIKECOIN_NODE_REST_API_BASE_URL` default value to PikaSer API

--- config/env.rb
+++ config/env.rb
@@ -2,9 +2,10 @@ ENV!.config do
   use(
     :LIKECOIN_NODE_REST_API_BASE_URL,
     <<~DESC.strip,
-      Default value from https://cosmos.directory/likecoin/nodes
+      Can use value from https://cosmos.directory/likecoin/nodes
       You can use your own API endpoint (even a local one)
     DESC
-    default: "https://rest.cosmos.directory/likecoin",
+    # Default to PikaSer API to avoid rate limit
+    default: "https://rest-likecoin-mainnet.pikaser.net",
   )
 end
