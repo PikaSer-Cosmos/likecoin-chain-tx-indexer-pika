79f06f25122d0730dafb67a2798a3df1bf4fbf6e
PikachuEXE <git@pikachuexe.net>
2023-12-21 10:28:07 +0800

    $ Remove usage of deprecated value

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -3,7 +3,7 @@ class ApplicationJob < ActiveJob::Base
   include GoodJob::ActiveJobExtensions::InterruptErrors
 
   # Retry job ONCE if got error (could be network related)
-  retry_on StandardError, wait: :exponentially_longer, attempts: 1
+  retry_on StandardError, wait: :polynomially_longer, attempts: 1
   # Retry the job if it is interrupted
   retry_on GoodJob::InterruptError, wait: 0, attempts: Float::INFINITY
 
