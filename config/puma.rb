86f124f49986a914cf5ed2c4beabb345f96c8889
PikachuEXE <git@pikachuexe.net>
2024-11-12 09:34:03 +0800

    * Update files via rails app:update

--- config/puma.rb
+++ config/puma.rb
@@ -4,12 +4,17 @@
 
 rails_env = ENV.fetch("RAILS_ENV", "development")
 
+#
 # Puma starts a configurable number of processes (workers) and each process
 # serves each request in a thread from an internal thread pool.
 #
+# You can control the number of workers using ENV["WEB_CONCURRENCY"]. You
+# should only set this value when you want to run 2 or more workers. The
+# default is already 1.
+#
 # The ideal number of threads per worker depends both on how much time the
 # application spends waiting for IO operations and on how much you wish to
-# to prioritize throughput over latency.
+# prioritize throughput over latency.
 #
 # As a rule of thumb, increasing the number of threads will increase how much
 # traffic a given process can handle (throughput), but due to CRuby's
@@ -22,7 +27,7 @@ rails_env = ENV.fetch("RAILS_ENV", "development")
 # Any libraries that use a connection pool or another resource pool should
 # be configured to provide at least as many connections as the number of
 # threads. This includes Active Record's `pool` parameter in `database.yml`.
-threads_count = ENV.fetch("RAILS_MAX_THREADS") { 3 }
+threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
 
 # Specifies that the worker count should equal the number of processors in production.
@@ -47,7 +52,7 @@ if rails_env == "production"
 end
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
-port ENV.fetch("PORT") { 3000 }
+port ENV.fetch("PORT", 3000)
 
 # Specifies the `environment` that Puma will run in.
 environment rails_env
